5e665030bc057615988b71f6f6e8974b32bd79de
daniil_asanov <huterbaka2008@mail.ru>
2026-06-23 11:26:37 +0600

    implement ISpaceship interface with Cruiserand Fighter

---
+++ task04/Cruiser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task04
+{
+    public class Cruiser : ISpaceship
+    {
+        public int Speed => 50;
+        public int FirePower => 100;
+
+        public void MoveForward()
+        {
+            Console.WriteLine("Cruiser moves forward slowly.");
+        }
+
+        public void Rotate(int angle)
+        {
+            Console.WriteLine($"Cruiser rotates {angle} degrees.");
+        }
+
+        public void Fire()
+        {
+            Console.WriteLine("Cruiser fires a powerful photon rocket!");
+        }
+    }
+}
