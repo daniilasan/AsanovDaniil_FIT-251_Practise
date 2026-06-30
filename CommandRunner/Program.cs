ecb73aa44871531e5d1f44843502bad08974e9d9
daniil_asanov <huterbaka2008@mail.ru>
2026-06-30 16:10:45 +0600

    implement dynamic library loading with reflection

---
+++ CommandRunner/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using CommandLib;
+using FileSystemCommands;
+
+namespace CommandRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string DllPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FileSystemCommands.dll");
+
+            if (File.Exists(DllPath) == false)
+            {
+                Console.WriteLine("DLL not found: " + DllPath);
+                return;
+            }
+
+            //Динамическая загрузка библиотеки через рефлексию
+            Assembly LoadedAssembly = Assembly.LoadFrom(DllPath);
+            Type[] Types = LoadedAssembly.GetTypes();
+
+            foreach (Type CurrentType in Types)
+            {
+                if (CurrentType.IsClass == true && CurrentType.IsAbstract == false)
+                {
+                    Type[] Interfaces = CurrentType.GetInterfaces();
+
+                    foreach (Type Interface in Interfaces)
+                    {
+                        if (Interface == typeof(ICommand))
+                        {
+                            Console.WriteLine("=== Found command: " + CurrentType.Name + " ===");
+
+                            object? CommandInstance = null;
+
+                            //Создаём экземпляры с нужными параметрами
+                            if (CurrentType.Name == "DirectorySizeCommand")
+                            {
+                                string TestPath = Path.GetTempPath();
+                                CommandInstance = Activator.CreateInstance(CurrentType, TestPath);
+                            }
+                            else if (CurrentType.Name == "FindFilesCommand")
+                            {
+                                string TestPath = Path.GetTempPath();
+                                string Mask = "*.txt";
+                                CommandInstance = Activator.CreateInstance(CurrentType, TestPath, Mask);
+                            }
+
+                            if (CommandInstance != null)
+                            {
+                                //Выполняем через интерфейс
+                                ICommand Command = (ICommand)CommandInstance;
+                                Command.Execute();
+
+                                //Приведение типа для получения результата 
+                                if (CommandInstance is DirectorySizeCommand SizeCommand)
+                                {
+                                    Console.WriteLine("Result: total size = " + SizeCommand.TotalSize + " bytes");
+                                }
+                                else if (CommandInstance is FindFilesCommand FindCommand)
+                                {
+                                    Console.WriteLine("Result: found " + FindCommand.FoundFiles.Length + " files");
+                                }
+
+                                Console.WriteLine();
+                            }
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ task08tests/FileSystemCommandsTests.cs
@@ -0,0 +1,110 @@
+﻿using Xunit;
+using FileSystemCommands;
+using System.IO;
+using System;
+
+namespace task08tests
+{
+    public class FileSystemCommandsTests
+    {
+        [Fact]
+        public void DirectorySizeCommand_ShouldCalculateSize()
+        {
+            string TestDir = Path.Combine(Path.GetTempPath(), "TestDir_" + Guid.NewGuid().ToString());
+            Directory.CreateDirectory(TestDir);
+
+            File.WriteAllText(Path.Combine(TestDir, "test1.txt"), "Hello");
+            File.WriteAllText(Path.Combine(TestDir, "test2.txt"), "World");
+
+            DirectorySizeCommand Command = new DirectorySizeCommand(TestDir);
+            Command.Execute();
+            Assert.True(Command.TotalSize > 0);
+
+            Directory.Delete(TestDir, true);
+        }
+
+        [Fact]
+        public void FindFilesCommand_ShouldFindMatchingFiles()
+        {
+            string TestDir = Path.Combine(Path.GetTempPath(), "TestDir_" + Guid.NewGuid().ToString());
+            Directory.CreateDirectory(TestDir);
+
+            File.WriteAllText(Path.Combine(TestDir, "file1.txt"), "Text");
+            File.WriteAllText(Path.Combine(TestDir, "file2.log"), "Log");
+            File.WriteAllText(Path.Combine(TestDir, "file3.txt"), "More text");
+
+            FindFilesCommand Command = new FindFilesCommand(TestDir, "*.txt");
+            Command.Execute();
+            Assert.Equal(2, Command.FoundFiles.Length);
+
+            Directory.Delete(TestDir, true);
+        }
+
+        [Fact]
+        public void DirectorySizeCommand_EmptyDirectory_ReturnsZero()
+        {
+            string TestDir = Path.Combine(Path.GetTempPath(), "EmptyDir_" + Guid.NewGuid().ToString());
+            Directory.CreateDirectory(TestDir);
+
+            DirectorySizeCommand Command = new DirectorySizeCommand(TestDir);
+            Command.Execute();
+
+            Assert.Equal(0, Command.TotalSize);
+
+            Directory.Delete(TestDir, true);
+        }
+
+        [Fact]
+        public void FindFilesCommand_NoMatchingFiles_ReturnsEmpty()
+        {
+            string TestDir = Path.Combine(Path.GetTempPath(), "TestDir_" + Guid.NewGuid().ToString());
+            Directory.CreateDirectory(TestDir);
+
+            File.WriteAllText(Path.Combine(TestDir, "file.log"), "Log");
+
+            FindFilesCommand Command = new FindFilesCommand(TestDir, "*.txt");
+            Command.Execute();
+            Assert.Empty(Command.FoundFiles);
+
+            Directory.Delete(TestDir, true);
+        }
+
+        [Fact]
+        public void DirectorySizeCommand_WithSubdirectories_ShouldIncludeNestedFiles()
+        {
+            string TestDir = Path.Combine(Path.GetTempPath(), "TestDir_" + Guid.NewGuid().ToString());
+            string SubDir = Path.Combine(TestDir, "SubDir");
+            Directory.CreateDirectory(TestDir);
+            Directory.CreateDirectory(SubDir);
+
+            File.WriteAllText(Path.Combine(TestDir, "root.txt"), "Root file content");
+            File.WriteAllText(Path.Combine(SubDir, "nested.txt"), "Nested file content");
+
+            DirectorySizeCommand Command = new DirectorySizeCommand(TestDir);
+            Command.Execute();
+            long ExpectedMinSize = "Root file content".Length + "Nested file content".Length;
+            Assert.True(Command.TotalSize >= ExpectedMinSize);
+
+            Directory.Delete(TestDir, true);
+        }
+
+        [Fact]
+        public void FindFilesCommand_WithSubdirectories_ShouldFindNestedFiles()
+        {
+            string TestDir = Path.Combine(Path.GetTempPath(), "TestDir_" + Guid.NewGuid().ToString());
+            string SubDir = Path.Combine(TestDir, "SubDir");
+            Directory.CreateDirectory(TestDir);
+            Directory.CreateDirectory(SubDir);
+
+            File.WriteAllText(Path.Combine(TestDir, "root.txt"), "Root");
+            File.WriteAllText(Path.Combine(SubDir, "nested.txt"), "Nested");
+            File.WriteAllText(Path.Combine(SubDir, "other.log"), "Log");
+
+            FindFilesCommand Command = new FindFilesCommand(TestDir, "*.txt");
+            Command.Execute();
+            Assert.Equal(2, Command.FoundFiles.Length);
+
+            Directory.Delete(TestDir, true);
+        }
+    }
+}
