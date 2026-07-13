e6c8d08e208934e875d7bc6ca0ca2fbd2bc1d449
daniil_asanov <huterbaka2008@mail.ru>
2026-07-13 10:03:56 +0600

    implement server thread with command queue and stop commands

---
+++ task17/HardStopCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace task17
+{
+    public class HardStopCommand : ICommand
+    {
+        private ServerThread TargetServer;
+
+        public HardStopCommand(ServerThread TargetServer)
+        {
+            this.TargetServer = TargetServer;
+        }
+
+        public void Execute()
+        {
+            if (Thread.CurrentThread != TargetServer.GetThread())
+            {
+                throw new InvalidOperationException("HardStop must be executed in the target thread");
+            }
+
+            TargetServer.RequestHardStop();
+        }
+    }
+}
